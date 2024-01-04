48c16b4ca6c55496deda23e42a2a82901301c2b3
fh-fahad <fahadbin8900@gmail.com>
2024-01-04 23:01:58 +0600

    API changed

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": "https://tasks-backend-one.vercel.app",
-    },
-  },
   plugins: [react()],
 });
